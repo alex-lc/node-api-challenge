f2221e9f4d515d85ba34db85e7404742b8437376
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-02-14 11:00:28 -0600

    POST to create projects, PUT for editing projects, and DELETE for deleting projects complete

--- data/projects/projectsRouter.js
+++ data/projects/projectsRouter.js
@@ -21,6 +21,51 @@ router.get('/:id', (req, res) => {
         .catch(err => {
             res.status(500).json({ error: "Something went wrong." });
         })
+});
+
+// post to create a new project
+router.post('/', (req, res) => {
+
+    const newPost = req.body;
+
+    Projects.insert(newPost)
+        .then(post => {
+            res.status(201).json(newPost);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Something went wrong." });
+        })
+
+});
+
+// put to modify a project by id
+router.put('/:id', (req, res) => {
+
+    const { id } = req.params;
+    const editedProject = req.body;
+
+    Projects.update(id, editedProject)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Something went wrong." });
+        })
 })
+
+// remove to delete a project by id
+router.delete('/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    Projects.remove(id)
+        .then(removed => {
+            res.status(200).json(removed);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Something went wrong." });
+        })
+
+});
 
 module.exports = router;
